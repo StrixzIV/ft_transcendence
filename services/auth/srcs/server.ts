c2ce85bac0d0bc30a2396e0c83351c2a8b33ed65
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2025-06-27 13:55:51 +0700

    (feat): Redirect all auth log into logs directory for ELK

--- services/auth/srcs/server.ts
+++ services/auth/srcs/server.ts
@@ -1,9 +1,16 @@
+import fs from 'fs';
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 
 import { userRoute } from './routes/user';
 
-const app = Fastify({ logger: true });
+const log_filestream = fs.createWriteStream('../../logs/auth.log', { flags: 'a' })
+
+const app = Fastify({ 
+    logger: {
+        stream: log_filestream
+    }
+});
 
 app.register(cors, {
     origin: '*'
