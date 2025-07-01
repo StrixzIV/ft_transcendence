b3612c163c88f3a6b6bfaf90d3b259aaacb629bf
monsieurjune <june.monsieurjune@gmail.com>
2025-07-01 20:22:19 +0700

    (feat): Add hasicorp's environment variable in generate_env.sh
    Feat:
    - Add VAULT_DEV_ROOT_TOKEN_ID, generate via openssl
    - Add JWT_ACCESS_TOKEN_SECRETS, generate via openssl
    - Add JWT_REFRESH_TOKEN_SECRETS, generate via openssl
    
    Fix:
    - Fix no newline after typing password is done

--- scripts/generate_env.sh
+++ scripts/generate_env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+TOKEN_SIZE=64
+
 append_env() {
     echo $1=$2 >> .env
 }
@@ -13,14 +15,21 @@ else
     touch .env
 	chmod 600 ./.env
 
-    # Read user's input
+    # Get env
     echo "============== CREATE ENV ================"
     printf "%s" "RABBITMQ_DEFAULT_USER: "; read RABBITMQ_DEFAULT_USER
-    printf "%s" "RABBITMQ_DEFAULT_PASS: "; read -s RABBITMQ_DEFAULT_PASS
+    printf "%s" "RABBITMQ_DEFAULT_PASS: "; read -s RABBITMQ_DEFAULT_PASS; echo
+
+    VAULT_DEV_ROOT_TOKEN_ID=$(openssl rand -base64 $TOKEN_SIZE)
+    JWT_ACCESS_TOKEN_SECRETS=$(openssl rand -base64 $TOKEN_SIZE)
+    JWT_REFRESH_TOKEN_SECRETS=$(openssl rand -base64 $TOKEN_SIZE)
 
     # Construct env
     append_env RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_USER
     append_env RABBITMQ_DEFAULT_PASS $RABBITMQ_DEFAULT_PASS
+    append_env VAULT_DEV_ROOT_TOKEN_ID $VAULT_DEV_ROOT_TOKEN_ID
+    append_env JWT_ACCESS_TOKEN_SECRETS $JWT_ACCESS_TOKEN_SECRETS
+    append_env JWT_REFRESH_TOKEN_SECRETS $JWT_REFRESH_TOKEN_SECRETS
 
 	echo "Done! .env is located at ./"
 fi
