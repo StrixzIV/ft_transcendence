1c8aecca2d5eb1cfed1db1d7e81254470c3487d5
monsieurjune <june.monsieurjune@gmail.com>
2025-07-03 04:54:29 +0700

    fix(vault-init): Unset env after using them

--- services/vault/scripts/jwt_vault.sh
+++ services/vault/scripts/jwt_vault.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# Fail on any error
+# Exit on any error
 set -e
 
-export VAULT_TOKEN="$VAULT_DEV_ROOT_TOKEN_ID"
+# export vault's token to env
+export VAULT_TOKEN=$VAULT_DEV_ROOT_TOKEN_ID
 
 echo "[vault-init] Waiting for Vault to be ready..."
 
@@ -14,9 +15,15 @@ done
 
 echo "[vault-init] Vault is ready."
 
-echo "[vault-init] Generated random JWT key."
+echo "[vault-init] Storing tokens..."
 
 # Store in Vault KV
-vault kv put secret/jwt access_secret="$JWT_ACCESS_TOKEN_SECRETS" refresh_secret="$JWT_REFRESH_TOKEN_SECRETS" algorithm="HS256"
+vault kv put secret/jwt \
+                access_secret="$JWT_ACCESS_TOKEN_SECRETS" \
+                refresh_secret="$JWT_REFRESH_TOKEN_SECRETS" \
+                algorithm="HS256"
+
+# Remove vault's token in env after use
+unset VAULT_TOKEN
 
 echo "[vault-init] Secret stored successfully!"
