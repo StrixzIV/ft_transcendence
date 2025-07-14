0eda504d5c78d328c7657e20ccf561f2c371356c
monsieurjune <june.monsieurjune@gmail.com>
2025-07-14 12:01:06 +0700

    feat(labratory): Add /date websocket

--- services/labratory/srcs/server.ts
+++ services/labratory/srcs/server.ts
@@ -1,14 +1,22 @@
 import Fastify from 'fastify';
+import fastifyWebsocket from '@fastify/websocket';
 
 import logger from '@/utils/logger'
 import greetingRoute from '@/routes/greeting';
+import dateRoute from '@/routes/date';
 
 async function createServer() {
     const loggerEngine = logger();
     const app = Fastify({ logger: loggerEngine });
 
+    // Register plugin
+    app.register(fastifyWebsocket);
+
     // API register point
     app.register(greetingRoute);
+
+    // WS register point
+    app.register(dateRoute);
 
     return app;
 }
