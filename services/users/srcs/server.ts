21c06d73708f49b045c39e77ebeb13677abbe5bf
Jirayu Kaewsing <strixz.self@gmail.com>
2025-07-17 02:16:42 +0700

    (users-backend): Adding /user/data for fetching userdata

--- services/users/srcs/server.ts
+++ services/users/srcs/server.ts
@@ -3,12 +3,12 @@ import fs from 'fs';
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import cookie from '@fastify/cookie';
-import { FastifyRequest, FastifyReply } from 'fastify';
 
 import { consumeMQData } from './utils/rabbitmq'
+import { userRoute } from './routes/user';
 
 const log_filestream = fs.createWriteStream('/logs/users.log', { flags: 'a' })
-const endpoint_prefix = '/users'
+const endpoint_prefix = '/user'
 
 async function initialize_server() {
 
@@ -25,9 +25,9 @@ async function initialize_server() {
     app.register(cookie)
 
     // API register point
-    // app.register(userRoute, {
-    //     prefix: endpoint_prefix
-    // });
+    app.register(userRoute, {
+        prefix: endpoint_prefix
+    });
 
     return app
 
