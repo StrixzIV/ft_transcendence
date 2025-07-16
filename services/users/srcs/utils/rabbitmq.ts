13478c7cda03978a612059d2f75e4863d4f68790
Jirayu Kaewsing <strixz.self@gmail.com>
2025-07-17 02:16:42 +0700

    (users-backend): Adding /user/data for fetching userdata

--- services/users/srcs/utils/rabbitmq.ts
+++ services/users/srcs/utils/rabbitmq.ts
@@ -1,7 +1,6 @@
 import amqp from 'amqplib';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../db';
+import { v4 as uuidv4 } from "uuid";
 
 export async function consumeMQData() {
 
@@ -42,4 +41,60 @@ export async function consumeMQData() {
             console.error('[Consumer Error]', error);
         }
     }, { noAck: true });
+}
+
+export async function JWTValidate(token: string) {
+
+    const conn = await amqp.connect(`amqp://${process.env.RABBITMQ_DEFAULT_USER ?? ''}:${process.env.RABBITMQ_DEFAULT_PASS ?? ''}@broker:5672`);
+    const channel = await conn.createChannel();
+
+    const correlationId = uuidv4();
+    const replyQueue = await channel.assertQueue('', { exclusive: true });
+
+    return new Promise<any>((resolve, reject) => {
+
+        let settled = false;
+
+        // Consume the reply
+        const consumerTagPromise = channel.consume(
+            replyQueue.queue,
+            (msg) => {
+                if (msg?.properties.correlationId === correlationId) {
+                if (settled) return;
+                settled = true;
+
+                const result = JSON.parse(msg.content.toString());
+                resolve(result);
+
+                // Cleanup: cancel consumer and delete queue
+                channel.cancel(msg.fields.consumerTag).catch(console.error);
+                channel.deleteQueue(replyQueue.queue).catch(console.error);
+                }
+            },
+            { noAck: true }
+        );
+
+        // Publish request
+        channel.sendToQueue(
+            "rpc.validate-jwt",
+            Buffer.from(JSON.stringify({ access_token: token })),
+            {
+                correlationId,
+                replyTo: replyQueue.queue,
+            }
+        );
+
+        // Timeout
+        setTimeout(async () => {
+            if (settled) return;
+            settled = true;
+            reject(new Error("JWT validation timeout"));
+
+            // Cancel consumer and delete queue (but DO NOT close channel or connection)
+            const { consumerTag } = await consumerTagPromise;
+            channel.cancel(consumerTag).catch(console.error);
+            channel.deleteQueue(replyQueue.queue).catch(console.error);
+        }, 3000);
+
+    });
 }
--- services/users/srcs/utils/vault_client.ts
+++
@@ -1,7 +0,0 @@
-import vaultFactory from "node-vault";
-
-export const vault = vaultFactory({
-    apiVersion: "v1", // default
-    endpoint: "http://vault:8200",
-    token: process.env.VAULT_DEV_ROOT_TOKEN_ID,
-});
