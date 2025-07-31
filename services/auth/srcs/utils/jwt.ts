4429355344d68c322e062b65a810088dd42cf483
monsieurjune <june.monsieurjune@gmail.com>
2025-07-31 15:49:03 +0700

    feat(auth-backend): Move google secrets to vault

--- services/auth/srcs/utils/jwt.ts
+++ services/auth/srcs/utils/jwt.ts
@@ -6,12 +6,10 @@ export interface JWTSecret {
 }
 
 export async function get_JWT_secret(): Promise<JWTSecret> {
-
     const result = await vault.read('secret/data/jwt');
 
     return {
         access_secret: result.data.data.access_secret,
         refresh_secret: result.data.data.refresh_secret
     } as JWTSecret;
-
 }
