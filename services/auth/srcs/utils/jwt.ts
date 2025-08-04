044ffe27212ac4bc4bdc5528a69bfe04c493ca6b
monsieurjune <june.monsieurjune@gmail.com>
2025-08-05 05:49:26 +0700

    feat(auth-backend): Move broker secrets to vault
    Feat:
    - Move RABBITMQ_DEFAULT_USER, RABBITMQ_DEFAULT_PASS to vault, except in
      broker part that still read via env file
    - Memorize vault's secrets in process, instead of constantly fetch

--- services/auth/srcs/utils/jwt.ts
+++ services/auth/srcs/utils/jwt.ts
@@ -5,11 +5,19 @@ export interface JWTSecret {
     refresh_secret: string
 }
 
+let jwt_secret: JWTSecret | null = null;
+
 export async function get_JWT_secret(): Promise<JWTSecret> {
+    if (jwt_secret) {
+        return jwt_secret;
+    }
+
     const result = await vault.read('secret/data/jwt');
 
-    return {
+    jwt_secret = {
         access_secret: result.data.data.access_secret,
         refresh_secret: result.data.data.refresh_secret
     } as JWTSecret;
+
+    return jwt_secret;
 }
