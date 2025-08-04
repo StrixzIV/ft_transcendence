706496ce4517a8201d738411a70c2aa2378ff6f9
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2025-08-04 16:29:11 +0700

    (users-backend): Add fetch userdata and image by UID

--- services/users/srcs/routes/user.ts
+++ services/users/srcs/routes/user.ts
@@ -30,7 +30,44 @@ export async function userRoute(fastify: FastifyInstance) {
             for (let attempt = 0; attempt < 5; attempt++) {
                 const user = await prisma.users.findUnique({ where: { id: uid } });
                 if (user) return user;
-                await new Promise(r => setTimeout(r, 200));
+                await new Promise(r => setTimeout(r, 50));
+            }
+
+            return null;
+        
+        } 
+        
+        catch (err) {
+            response.status(500).send({ error: "JWT validation failed" });
+        }
+
+    })
+
+    fastify.get('/data/:uid', async (request, response) => {
+
+        const { uid } = request.params as { uid: string };
+        const token = request.cookies["access_token"];
+
+        if (!token) {
+            return response.status(401).send({ error: "Missing token" });
+        }
+
+        if (!uid) {
+            return response.status(400).send({ error: "Missing UID" });
+        }
+
+        try {
+
+            const result = await JWTValidate(token);
+            
+            if (!result.valid) {
+                return response.status(401).send({ error: "Invalid token" });
+            }
+
+            for (let attempt = 0; attempt < 5; attempt++) {
+                const user = await prisma.users.findUnique({ where: { id: uid } });
+                if (user)  return user;
+                await new Promise(r => setTimeout(r, 50));
             }
 
             return null;
@@ -78,6 +115,63 @@ export async function userRoute(fastify: FastifyInstance) {
             const result = await s3.send(command);
             const buffer = await stream_to_buf(result.Body as Readable);
 
+            response.header('Cache-Control', 'public, max-age=30');
+
+            response
+                .header("Content-Type", result.ContentType)
+                .header("Content-Length", buffer.length)
+                .send(buffer);
+
+        }
+        
+        catch (err) {
+            request.log.error(err);
+            response.status(500).send({ error: "Failed to fetch image" });
+        }
+
+    })
+
+    fastify.get('/image/:uid', async (request, response) => {
+
+        const token = request.cookies["access_token"];
+        const { uid } = request.params as { uid: string };
+
+        if (!token) {
+            return response.status(401).send({ error: "Missing token" });
+        }
+
+        if (!uid) {
+            return response.status(400).send({ error: "Missing UID" });
+        }
+
+        try {
+
+            const token_data = await JWTValidate(token);
+            
+            if (!token_data.valid) {
+                return response.status(401).send({ error: "Invalid token" });
+            }
+
+            const user = await prisma.users.findUnique({ where: { id: uid } });
+
+            let images_name = "default-profile.png";
+
+            if (user) {
+                images_name = user.profile_url ?? "default-profile.png";
+            }
+
+            console.log(images_name)
+
+            const command = new GetObjectCommand({
+                Bucket: "ft-transendence-images",
+                Key: images_name
+            });
+            
+            const result = await s3.send(command);
+            const buffer = await stream_to_buf(result.Body as Readable);
+
+            response.header('Cache-Control', 'public, max-age=30');
+
             response
                 .header("Content-Type", result.ContentType)
                 .header("Content-Length", buffer.length)
@@ -110,7 +204,7 @@ export async function userRoute(fastify: FastifyInstance) {
         if (!data.mimetype.startsWith("image/")) {
             return response.status(400).send({ error: "Only image uploads are allowed" });
         }
-        
+
         const chunks: Buffer[] = [];
         
         for await (const chunk of data.file) {
