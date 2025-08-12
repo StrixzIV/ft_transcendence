f39927fdceab0f86a49fdf961fe2504cfd7028ca
Natachat Teecharcharn <techazuza@gmail.com>
2025-08-12 15:30:06 +0700

    feat: finalize the style for tournament pages

--- frontend/src/tournament/tournament_init.ts
+++ frontend/src/tournament/tournament_init.ts
@@ -5,10 +5,10 @@ export function tournamentSetupPage() {
     document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         <!-- Player Input and List Section -->
         <div id="player-setup" class="m-8 p-6 card text-xl">
-            <h2 class="mb-4 text-large font-semibold">ADD PLAYERS</h2>
+            <h2 class="mb-4 text-large font-semibold">INITIALIZE TOURNAMENT</h2>
             <div class="flex gap-4">
                 <input type="text" id="player-name-input" placeholder="Enter player name" class="form-field w-4/5">
-                <button id="add-player-btn" class="bg-[#444] hover:bg-[#555] disabled:bg-gray-400 transition font-semibold py-2 rounded w-1/5 border border-[#555]">Add Player</button>
+                <button id="add-player-btn" class="bg-[#444] hover:bg-[#555] disabled:bg-gray-400 transition font-semibold py-2 rounded w-1/5 border border-[#555]">ADD PLAYER</button>
             </div>
             <div id="player-list-container" class="mt-4">
                 <h3 class="text-lg font-medium mb-2">Players Added (<span id="player-count">0</span>/4):</h3>
@@ -17,7 +17,7 @@ export function tournamentSetupPage() {
                 </ul>
             </div>
             <div class="mt-6 flex justify-center">
-                <button id="start-tournament-btn" class="bg-green-600 text-white disabled:text-[#999] font-bold py-3 px-8 rounded-lg shadow-md hover:bg-green-700 transition-colors disabled:bg-[#222] border border-[#444]" disabled>Start Tournament</button>
+                <button id="start-tournament-btn" class="bg-green-600 text-white disabled:text-[#999] font-bold py-3 px-8 rounded-lg shadow-md hover:bg-green-700 transition-colors disabled:bg-[#222] border border-[#444]" disabled>START TOURNAMENT</button>
             </div>
         </div>
     `;
