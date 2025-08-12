a8396e471c823a6094e0deca05c84bd1d007bc79
Jirayu Kaewsing <strixz.self@gmail.com>
2025-08-12 21:41:53 +0700

    (game): Make game rejoinable

--- services/pingpong/srcs/game/room.ts
+++ services/pingpong/srcs/game/room.ts
@@ -3,11 +3,25 @@ import type { WebSocket } from 'ws';
 import { Ball } from './ball';
 import { Paddle } from './paddle';
 
+export interface PlayerInfo {
+    playerId: 'player1' | 'player2';
+    uid: string;
+    username: string;
+}
+
+export interface PlayerState {
+    ws: WebSocket | null;
+    playerId: 'player1' | 'player2';
+    uid: string;
+    username: string;
+}
+
 export interface GameRoom {
     gid: string;
     player1: WebSocket | null;
     player2: WebSocket | null;
-    players: Map<WebSocket, 'player1' | 'player2'>;
+    players: Map<WebSocket, PlayerInfo>;
+    playersState: Map<string, PlayerState>;
     leftPaddle: Paddle;
     rightPaddle: Paddle;
     ball: Ball;
