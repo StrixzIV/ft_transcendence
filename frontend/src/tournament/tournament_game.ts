756f4a7ed1f43c186b303a29edf9eef197fae8d2
Jirayu Kaewsing <jikaewsi@z3t3c1.42bangkok.com>
2025-08-12 20:17:42 +0700

    (frontend): change game fonts

--- frontend/src/tournament/tournament_game.ts
+++ frontend/src/tournament/tournament_game.ts
@@ -1,6 +1,6 @@
 import { navigate } from "../router";
 import { Paddle, Ball } from "./pong_paddle_ball";
-import scoreFontUrl from './pong-score.otf.woff2?url';
+import scoreFontUrl from './Pixel-UniCode.ttf?url';
 
 export class Pong {
 
@@ -105,7 +105,7 @@ export class Pong {
             this._ballSpeed, -this._ballSpeed
         );
 
-        const myFont = new FontFace('pong-score', `url(${scoreFontUrl}) format('woff2')`);
+        const myFont = new FontFace('pong-score', `url(${scoreFontUrl}) format('truetype')`);
 
         myFont.load().then(function(loadedFont) {
             document.fonts.add(loadedFont);
