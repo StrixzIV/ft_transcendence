d8f7f4df796f3b166365c8b41e3b752f414fe486
Natachat Teecharcharn <techazuza@gmail.com>
2025-08-15 16:48:51 +0700

    fix: fix Game Log win/loss results not aligning

--- frontend/src/main-components/game_log_card.ts
+++ frontend/src/main-components/game_log_card.ts
@@ -5,44 +5,31 @@ import { type User } from '../interfaces/user.ts'
 import { type Match } from '../interfaces/match.ts'
 
 export async function gameLogCard() {
-
-    const history = await secureFetch(game_endpoint('/stats/history'), {
-        method: 'GET'
-    });
-
+    const history = await secureFetch(game_endpoint('/stats/history'), { method: 'GET' });
     const match_data = await history.json() as Array<Match>;
 
     let gameLogItems = '';
 
     for (const match_item of match_data) {
+        const leftPlayer = match_item.match.players.find(player => player.side === 'leftPlayer')!;
+        const leftPlayerRes = await secureFetch(users_endpoint(`/data/${leftPlayer?.uid}`), { method: 'GET' });
+        const leftPlayerData = await leftPlayerRes.json() as User;
+
+        const rightPlayer = match_item.match.players.find(player => player.side === 'rightPlayer')!;
+        const rightPlayerRes = await secureFetch(users_endpoint(`/data/${rightPlayer?.uid}`), { method: 'GET' });
+        const rightPlayerData = await rightPlayerRes.json() as User;
 
         const isWinner = match_item.uid === match_item.match.winner_id;
-        const resultClass = isWinner ? 'text-green' : 'text-red';
+        const resultTextColor = isWinner ? 'text-green' : 'text-red';
         const resultText = isWinner ? 'WIN' : 'LOSS';
-        const score = `${match_item.match.left_score}-${match_item.match.right_score}`;
-
-        const leftPlayer = match_item.match.players.find(player => player.side === 'leftPlayer')!;
-        const rightPlayer = match_item.match.players.find(player => player.side === 'rightPlayer')!;
-
-        const leftOpponent = await secureFetch(users_endpoint(`/data/${leftPlayer?.uid}`), {
-            method: 'GET'
-        });
-
-        const rightOpponent = await secureFetch(users_endpoint(`/data/${rightPlayer?.uid}`), {
-            method: 'GET'
-        });
-
-        const leftOpponentData = await leftOpponent.json() as User;
-        const rightOpponentData = await rightOpponent.json() as User;
-
+        const resultScores = `${match_item.match.left_score} - ${match_item.match.right_score}`;
         gameLogItems += `
-            <div class="game-log-row-item">
-            <span>${leftOpponentData.username} vs ${rightOpponentData.username}</span>
-            <span class="${resultClass}">${resultText}</span>
-            <span>${score}</span>
+            <div class="game-log-row">
+              <span class="game-log-row-left">${leftPlayerData.username} vs ${rightPlayerData.username}</span>
+              <span class="game-log-row-middle ${resultTextColor}">${resultText}</span>
+              <span class="game-log-row-right">${resultScores}</span>
             </div>
         `;
-
     }
 
     return `
